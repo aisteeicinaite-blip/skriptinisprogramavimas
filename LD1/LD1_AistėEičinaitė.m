563a5ca78d917ee5d477a961bf6abb12fe3300af
aisteeicinaite-blip <aiste.eicinaite@gmail.com>
2026-09-08 12:50:56 +0300

    Update LD1_AistėEičinaitė.m

--- LD1/LD1_AistėEičinaitė.m
+++ LD1/LD1_AistėEičinaitė.m
@@ -1,1 +1,13 @@
 % Aistė Eičinaitė, EEf-25/2, 2026-09-08
+%
+% Paprastas skriptas
+%
+
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-]   |   F_2 [-X-]')
+
