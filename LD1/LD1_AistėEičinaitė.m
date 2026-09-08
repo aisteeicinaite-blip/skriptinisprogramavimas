bf4313115f43420d767b4c40e47223dd4ea2d47f
aisteeicinaite-blip <aiste.eicinaite@gmail.com>
2026-09-08 13:43:24 +0300

    Update LD1_AistėEičinaitė.m

--- LD1/LD1_AistėEičinaitė.m
+++ LD1/LD1_AistėEičinaitė.m
@@ -30,3 +30,20 @@ docsearch title
 % išdėstytų taškų
 % size - masyvo arba matricos matmenų (dydžio) nustatymas
 % max - didžiausios reikšmės ir jos indekso paieška
+
+
+% papildoma uzd
+N = 4 
+v = (N + 1) : 0.5 : (N + 4)
+A = [N N+1 N+2; N+3 N+4 N+5; N+6 N+7 N+8]
+
+% a)
+A(3, 2)
+% b)
+A(2:3,1:2)
+% c)
+A([1 3], [1 3])
+
+
+v_1 = v(1:3);
+A_1 = [A; v_1]
