e691b75b47e050fe484e312fb6caadf51ac57991
aisteeicinaite-blip <aiste.eicinaite@gmail.com>
2026-09-08 13:12:00 +0300

    Update LD1_AistėEičinaitė.m

--- LD1/LD1_AistėEičinaitė.m
+++ LD1/LD1_AistėEičinaitė.m
@@ -11,3 +11,22 @@ title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-o-]   |   F_2 [-X-]')
 
+% 8 uzd
+help sin 
+help plot
+help title
+
+doc sin  
+doc plot
+doc title
+
+docsearch sin
+docsearch plot
+docsearch title
+
+% 9 uzd
+% linspace - tolygiai išdėstytų skaičių sekos (vektoriaus) generavimas
+% y = linspace(x1, x2)- generuoja 50 tolygiai 
+% išdėstytų taškų
+% size - masyvo arba matricos matmenų (dydžio) nustatymas
+% max - didžiausios reikšmės ir jos indekso paieška
