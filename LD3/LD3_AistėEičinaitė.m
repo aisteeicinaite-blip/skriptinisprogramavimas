c908cabd83477744ffd3fb402a9d2dfad9079345
aisteeicinaite-blip <aiste.eicinaite@gmail.com>
2026-09-22 13:26:40 +0300

    Update LD3_AistėEičinaitė.m

--- LD3/LD3_AistėEičinaitė.m
+++ LD3/LD3_AistėEičinaitė.m
@@ -46,3 +46,81 @@ ylabel('y');
 zlabel('A');
 title('Paviršiaus diagrama');
 grid on;
+
+%papildoma uzd 21 var
+rng(42);
+
+%duomenys
+A = 8;
+f = 5;
+sigma = 1.8;
+U1 = 5;
+U2 = 3;
+t = 0:0.005:1.5;
+
+n = sigma * randn(size(t));
+s = A * cos(2*pi*f*t) + n;
+
+s_atrinktas = s(s > U1);
+
+s_filtruotas = s;
+s_filtruotas(abs(s) < U2) = 0;
+
+figure('Name','Signalų grafinis atvaizdavimas');
+
+%a)
+subplot(1,2,1);
+
+plot(t,s,'--','LineWidth',1);
+hold on;
+plot(t,s_filtruotas,':','LineWidth',1);
+
+yline(U1,'--','Color',[0.5 0 0.5],'LineWidth',1);
+yline(U2,'--','Color',[0.5 0 0.5],'LineWidth',1);
+yline(-U2,'--','Color',[0.5 0 0.5],'LineWidth',1);
+
+xlabel('Laikas, s');
+ylabel('Įtampa, V');
+
+title('Pradinis ir filtruotas signalai', ...
+    'Color',[0.5 0 0.5], ...
+    'FontSize',14);
+
+legend('Pradinis signalas','Filtruotas signalas',...
+    'U_1','U_2','-U_2','Location','best');
+
+grid on;
+xlim([0 1.5]);
+
+
+%b)
+subplot(1,2,2);
+
+ind = s > U1;
+
+plot(t(ind),s(ind),'o','LineWidth',1);
+hold on;
+
+max_atrinktas = max(s_atrinktas);
+min_atrinktas = min(s_atrinktas);
+
+t_atrinktas = t(ind);
+
+t_max = t_atrinktas(s_atrinktas == max_atrinktas);
+t_min = t_atrinktas(s_atrinktas == min_atrinktas);
+
+plot(t_max,max_atrinktas,'g*','MarkerSize',10);
+plot(t_min,min_atrinktas,'r*','MarkerSize',10);
+
+xlabel('Laikas, s');
+ylabel('Įtampa, V');
+
+title('Reikšmės, viršijančios U_1', ...
+    'Color',[0.5 0 0.5], ...
+    'FontSize',14);
+
+legend('Atrinktos reikšmės','Maksimali reikšmė',...
+    'Minimali reikšmė','Location','best');
+
+grid on;
+xlim([0 1.5]);
